dc4c499cac0f9bd5ac99fa0439bad19314e956c5
muhammaddiyor <muhammaddiyor@gmail.com>
2026-01-28 14:04:58 +0500

    Loader

--- js/app.js
+++ js/app.js
@@ -3,75 +3,35 @@ import { loader } from './loader.js';
 import { ui } from './ui.js';
 
 let state = null;
-let edited = null
-
-function stateChanger(value) {
-  state = value;
-  ui(state);
-}
 
 loader(true);
 fetch('https://json-api.uz/api/project/muhammaddiyor-afandim/todos')
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    console.log(res);
-    stateChanger(res.data);
-  })
-  .catch((err) => {
+.then((res) => {
+  return res.json();
+})
+.then((res) => {
+  state=res.data;
+  stateChanger(res.data);
+})
+.catch((err) => {
     console.log('Xatolik bor ' + err);
   })
   .finally(() => {
     loader(false);
-  });
 
-function edits(data) {
-  fetch('https://json-api.uz/api/project/muhammaddiyor-afandim/todos/' + data.id, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      const result = state.map((el) => {
-        if (el.id === res.id) {
-          return res;
-        } else {
-          return el;
-        }
-      });
-      stateChanger(result);
-      editModal.close();
-    })
-    .catch((err) => {
-      console.log('Xatolik bor ' + err);
-    })
-    .finally(() => {});
+  });
+  
+function stateChanger(value) {
+  if(value!="get") {
+  state = value;
+  ui(state);
+  } else {
+    return state;
+  }
+  
 }
 
-elForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(elForm);
-  const result = { id: edited };
-  formData.forEach((value, key) => {
-    result[key] = value;
-  });
-  edits(result);
-});
 
-elTodosContainer.addEventListener('click', (evt) => {
-  if (evt.target.classList.contains('js-edit')) {
-    editModal.showModal();
-    const data = state.find((el) => el.id == evt.target.id);
-
-    elForm.status.value = data.status;
-    elForm.title.value = data.title;
-    elForm.text.value = data.text;
-    edited = data.id;
-  }
-});
+export {
+  stateChanger
+}
--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,5 @@
-import { elCardTemplate, elTodosContainer } from './html.js';
+import { stateChanger } from './app.js';
+import { elCardTemplate, elEdtForm, elTodosContainer } from './html.js';
 export function ui(todos) {
   if (!Array.isArray(todos)) return;
 
@@ -15,6 +16,92 @@ export function ui(todos) {
 
     clone.querySelector('.js-status').innerText = element.status || 'unknown';
 
+    clone.firstElementChild.setAttribute("data-card-id",element.id);
+
     elTodosContainer.appendChild(clone);
   });
+
+  document.querySelectorAll(".js-todo-card").forEach(el=>{
+    el.addEventListener("click",(evt)=>{
+      if(evt.target.classList.contains("js-edit")==true) {
+      editToDo(evt.target.closest(".js-todo-card").getAttribute("data-card-id"));
+      } else if(evt.target.classList.contains("js-delete")==true) {
+      deleteToDo(evt.target.closest(".js-todo-card").getAttribute("data-card-id"));
+      } 
+    })
+
+  })
+
+};
+
+function deleteToDo(id) {
+  fetch("https://json-api.uz/api/project/muhammaddiyor-afandim/todos/"+id,{
+    method:"DELETE"
+  }).then(res=>res.text())
+  .then(res=>{
+    if(res=="deleted successfully") {
+      alert("O'chirildi");
+      deleteToDoAfterUI(id);
+    } else {
+      alert("O'chirilmadi. qayta urining");
+    }
+  }).catch(
+    err=>{
+      console.log(err);
+      alert("O'chirilmadi. qayta urining");
+    }
+  )
+};
+ 
+function deleteToDoAfterUI(id) {
+  let oldState = stateChanger("get"); 
+  let newState = oldState.filter(el=>{
+    if(el.id!=id) return true;
+  });
+  stateChanger(newState);
+};
+
+function editToDo(id) {
+  fetch("https://json-api.uz/api/project/muhammaddiyor-afandim/todos/"+id)
+  .then(res=>res.json())
+  .then(res=>{
+      editFormFiller(res)
+  })
+  
+  elEdtForm.addEventListener("submit",(evt)=>{
+    evt.preventDefault();
+    let editData = new FormData(elEdtForm);
+    let reqObj ={
+      title: editData.get("title"),
+      text: editData.get("text"),
+      status: editData.get("status"),
+    };
+    fetch("https://json-api.uz/api/project/muhammaddiyor-afandim/todos/"+id,{
+      method:"PATCH",
+      headers:{
+        "Content-Type":"application/json"
+      }, body: JSON.stringify(reqObj)
+    }).then(res=>res.json())
+    .then(res=>{
+      editToDoAfterUI(res);
+    });
+  });
+  
 }
+
+function editFormFiller(data) {
+  editModal.showModal()
+  elEdtForm.querySelector('[name="title"]').value=data.title?data.title:"No title";
+  elEdtForm.querySelector('[name="text"]').value=data.text?data.text:"No text";
+  elEdtForm.querySelector('[name="status"]').value=data.status?data.status:"pending";
+}
+
+function editToDoAfterUI(data) {
+  editModal.close()  
+  let oldState = stateChanger("get"); 
+  let newState = oldState.map(el=>{
+    if(el.id!=data.id) return el;
+    else return data;
+  });
+  stateChanger(newState); 
+};
